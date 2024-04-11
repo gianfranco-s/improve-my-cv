9eb7f5199c33f991dbd4542ba1ea037345bfe4d8
gsalomone <gianfranco.s@gmail.com>
2024-04-11 12:22:36 -0300

    Test prompt creation

--- improve_my_cv/tests/test_prompt_creator.py
+++ improve_my_cv/tests/test_prompt_creator.py
@@ -1,5 +1,15 @@
 from improve_my_cv.prompt_creator import PromptCreator
 
 
-def test_prompt_creator_with_valid_strings():
-    prompt = PromptCreator()
+def test_prompt_creator_create_prompt_with_valid_strings():
+    prompt_template = "She's got a {job_description} reminds me {json_resume}"
+    job_description = 'smile that it seems to me'
+    json_resume = 'childhood memories'
+
+    prompt_creator = PromptCreator(prompt_template=prompt_template,
+                           job_description=job_description,
+                           json_resume=json_resume)
+    
+    full_prompt = prompt_creator.create_prompt()
+
+    assert full_prompt == prompt_template.format(job_description=job_description, json_resume=json_resume)
