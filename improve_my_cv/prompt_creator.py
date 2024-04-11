a4768c1b75b51a9b834cea41ed89917ed19fda67
gsalomone <gianfranco.s@gmail.com>
2024-04-11 14:50:18 -0300

    Fill out PromptCreator

--- improve_my_cv/prompt_creator.py
+++ improve_my_cv/prompt_creator.py
@@ -5,27 +5,23 @@ from dataclasses import dataclass
 class PromptCreator:
     job_description: str
     json_resume: str
-    prompt_template: str
-    # prompt_template: str = """
-    #     You'll be given two parameters: `job_description` and a `json_resume`. The first one is a job description text. The latter is a resume in the JSON format.
-    #     Please update `json_resume`'s fields to better match the `job_description`.
+    prompt_template: str = """
+        You'll be given two parameters: `job_description` and a `json_resume`. The first one is a job description text. The latter is a resume in the JSON format.
+        Please update `json_resume`'s fields to better match the `job_description`.
 
-    #     Each parameter will be delimited by the characters ```
+        Each parameter will be delimited by the characters ```
         
-    #     job_description = 
-    #     ```
-    #     {job_description}
-    #     ```
+        job_description = 
+        ```
+        {job_description}
+        ```
 
-    #     json_resume = 
-    #     ```
-    #     {json_resume}
-    #     ```
-    #     """
+        json_resume = 
+        ```
+        {json_resume}
+        ```
+        """
 
     def create_prompt(self) -> str:
-        pass
-
-
-class ImproveMyCV:
-    pass
+        return self.prompt_template.format(job_description=self.job_description,
+                                           json_resume=self.json_resume)
