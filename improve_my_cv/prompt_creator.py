a1ad64317e3b917e27cd6ac9b0b6a2b74b8aaaca
gsalomone <gianfranco.s@gmail.com>
2024-04-19 06:36:00 -0300

    Update prompt template

--- improve_my_cv/prompt_creator.py
+++ improve_my_cv/prompt_creator.py
@@ -7,20 +7,21 @@ class PromptCreator:
     json_resume: str
     prompt_template: str = """
         You'll be given two parameters: `job_description` and a `json_resume`. The first one is a job description text. The latter is a resume in the JSON format.
-        Please update `json_resume`'s fields to better match the `job_description`.
+        Please update the following sections of `json_resume`'s fields to better match the `job_description`: "work", "skills", "interests", "projects", "summary", "label"
+        **by including relevant keywords** from the job description.
+        
+        Please only answer with an modified JSON, based on `json_resume`
+        Do not provide any other explanation, outside the JSON response
+        Do not change any field names
+        Do not change any values for the following fields: "name", "email", "phone", "url", "location", "profiles", "education", "certificates", "publications", "languages"
+        Each parameter's value will be delimited by the characters ```
 
-        Additional instructions:
-        - Please only answer with an updated JSON
-        - The answer should only be a JSON string
-        - Do not provide any other explanation, outside the JSON response
-        - Each parameter will be delimited by the characters ```
-
-        job_description =
+        `job_description`:
         ```
         {job_description}
         ```
 
-        json_resume =
+        `json_resume`:
         ```
         {json_resume}
         ```
