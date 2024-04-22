a9489b2e8a1887937f8a4ef3b05032ef1640d2d5
gsalomone <gianfranco.s@gmail.com>
2024-04-22 12:03:08 -0300

    Add button to improve the CV

--- improve_my_cv/main.py
+++ improve_my_cv/main.py
@@ -1,9 +1,6 @@
 import json
-
 from pathlib import Path
-
 import streamlit as st
-
 from improve_my_cv import resume_dir
 from improve_my_cv.cv_improve import ImproveMyCV
 from improve_my_cv.log_config import logger
@@ -45,20 +42,21 @@ def streamlit_ui():
     job_description = st.text_area("Enter Job Description", height=200, placeholder=job_description_placeholder)
     uploaded_file = st.file_uploader("Upload your CV (JSON format)", type="json")
 
-    if uploaded_file is not None and job_description:
-        resume = json.load(uploaded_file)
+    if st.button("Improve!"):
+        if uploaded_file is not None and job_description:
+            resume = json.load(uploaded_file)
 
-        improve = ImproveMyCV(original_resume=resume, job_description=job_description)
-        improved_cv = improve.improve_cv()
+            improve = ImproveMyCV(original_resume=resume, job_description=job_description)
+            improved_cv = improve.improve_cv()
 
-        warnings = improve.response_warnings()
-        if warnings:
-            logger.warning(warnings)
-            st.warning(warnings)
+            warnings = improve.response_warnings()
+            if warnings:
+                logger.warning(warnings)
+                st.warning(warnings)
 
-        filename = 'improved_cv.json'
-        save_operations(improved_cv=improved_cv, filename=filename)
-        st.success(f"Improved CV has been saved to '{filename}'")
+            filename = 'improved_cv.json'
+            save_operations(improved_cv=improved_cv, filename=filename)
+            st.success(f"Improved CV has been saved to '{filename}'")
 
 
 if __name__ == "__main__":
