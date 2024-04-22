250bfbf4e314acf611bc23f60ad399d1f6df9679
gsalomone <gianfranco.s@gmail.com>
2024-04-22 18:53:45 -0300

    Set up llm for streamlit

--- improve_my_cv/ui_streamlit.py
+++ improve_my_cv/ui_streamlit.py
@@ -3,11 +3,12 @@ import json
 import streamlit as st
 
 from improve_my_cv.cv_improve import ImproveMyCV
+from improve_my_cv.llm_handlers.ollama import HandleOllama, DEFAULT_OLLAMA_MODEL
 from improve_my_cv.log_config import logger
 from improve_my_cv.utils import save_operations
 
 
-def streamlit_ui():
+def streamlit_ui(model: str = DEFAULT_OLLAMA_MODEL):
     st.title("Improve My CV")
 
     job_description = st.text_area("Enter Job Description", height=200, placeholder='Add a text job description here')
@@ -18,6 +19,9 @@ def streamlit_ui():
             resume = json.load(uploaded_file)
 
             improve = ImproveMyCV(original_resume=resume, job_description=job_description)
+            improve.llm_setup(model=model, llm_handler=HandleOllama())
+
+            st.write(f'Improving resume using model {model}')
             improved_cv = improve.improve_cv()
 
             warnings = improve.response_warnings()
