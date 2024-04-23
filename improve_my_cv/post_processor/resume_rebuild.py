fbc86c193d98f24d13726a80af2de4a3d5549314
gsalomone <gianfranco.s@gmail.com>
2024-04-23 12:39:37 -0300

    Create ResumeBuilder with unit tests

--- improve_my_cv/post_processor/resume_rebuild.py
+++ improve_my_cv/post_processor/resume_rebuild.py
@@ -1,9 +1,22 @@
-class ResumeRebuilder:
-    """Given an original resume and a filtered_resume, update the original one to include the filtered one"""
+import copy
 
+
+class ResumeRebuilder:
     def __init__(self, original_resume: dict, filtered_resume: dict) -> None:
-        self.original = original_resume
-        self.filtered = filtered_resume
+        self.original_resume = original_resume
+        self.filtered_resume = filtered_resume
 
     def rebuild(self) -> dict:
-        return self.filtered
+        rebuilt_resume = copy.deepcopy(self.original_resume)
+
+        for section_name, section_content in self.filtered_resume.items():
+            if section_name in rebuilt_resume:
+                if isinstance(section_content, dict):
+                    rebuilt_resume[section_name].update(section_content)
+                elif isinstance(section_content, list):
+                    original_items = rebuilt_resume[section_name]
+                    filtered_items = section_content
+                    for original_item, filtered_item in zip(original_items, filtered_items):
+                        original_item.update(filtered_item)
+
+        return rebuilt_resume
--- improve_my_cv/utils.py
+++ improve_my_cv/utils.py
@@ -1,6 +1,8 @@
 import json
 from pathlib import Path
 
+from deepdiff import DeepDiff
+
 from improve_my_cv.log_config import logger
 
 
@@ -14,3 +16,18 @@ def save_operations(improved_cv: dict, filename: str) -> None:
 
     with open(filename, 'w') as f:
         json.dump(improved_cv, f, indent=4)
+
+
+def are_keys_the_same(dict1: dict, dict2: dict) -> bool:
+    diff = DeepDiff(dict1, dict2, ignore_order=True)
+
+    if 'dictionary_item_added' in diff or 'dictionary_item_removed' in diff:
+        return False  # If dictionaries were added or removed, keys are not the same
+
+    nested_diffs = diff.get('values_changed', [])
+
+    for nested_diff in nested_diffs:
+        if 'new_value' in nested_diff or 'old_value' in nested_diff:
+            return False  # If values were changed within nested dictionaries, keys are not the same
+
+    return True
